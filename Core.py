3553ffef521a562dd1413aec76710e93aea247fa
carebear482 <osuuster@gmail.com>
2019-10-12 01:18:43 +0300

    added forward and sideways movements

--- Core.py
+++ Core.py
@@ -1,15 +1,33 @@
 #!/usr/bin/env python3
-from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_B, SpeedPercent, MoveTank
-from ev3dev2.sensor import INPUT_1
-from ev3dev2.sensor.lego import TouchSensor
-from ev3dev2.led import Leds
-from ev3dev2.sound import Sound
+from ev3dev2.motor import Motor, LargeMotor, OUTPUT_B, OUTPUT_C, SpeedPercent, MoveTank
+
+from time import sleep
 
 # TODO: Add code here
-def moveforward():
+left_motor = OUTPUT_B
+right_motor = OUTPUT_C
+tank_drive = MoveTank(left_motor, right_motor)
 
 
-def say(message):
-    if message:
-        Sound.speak(message)
+def move_forward():
+    speed = 75
+    rotations = 2
+    # Turn on left and right motor with defined speed for defined rotations
+    tank_drive.on_for_rotations(SpeedPercent(speed), SpeedPercent(speed), rotations)
 
+
+def turn(direction):
+    speed = 75
+    rotations = 2
+    # Turn on left and right motor with defined speed for defined rotations
+    if direction == 'left':
+        tank_drive.on_for_rotations(SpeedPercent(0), SpeedPercent(speed), rotations)
+    elif direction == 'right':
+        tank_drive.on_for_rotations(SpeedPercent(speed), SpeedPercent(0), rotations)
+
+#Cycle
+move_forward()
+
+turn('left')
+
+turn('right')

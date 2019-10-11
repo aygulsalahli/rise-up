8b8d1ea8e5cdd20d5141b10ac40216d26dd836aa
carebear482 <osuuster@gmail.com>
2019-10-11 22:06:05 +0300

    added gitignore and core.py

---
+++ Core.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_B, SpeedPercent, MoveTank
+from ev3dev2.sensor import INPUT_1
+from ev3dev2.sensor.lego import TouchSensor
+from ev3dev2.led import Leds
+from ev3dev2.sound import Sound
+
+# TODO: Add code here
+def moveforward():
+
+
+def say(message):
+    if message:
+        Sound.speak(message)
+

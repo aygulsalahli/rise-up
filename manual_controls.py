b0801c282bc5f98d7db290560b44508fff1b2059
Aygul Salahli <aygulsalahli@gmail.com>
2019-10-13 04:19:08 +0300

    add multhithreading

--- manual_controls.py
+++ manual_controls.py
@@ -1,11 +1,43 @@
 import getch
-done = False
-while not done:
-    char = getch.getch()
-    if char == ' ':
-        done = True
-    else:
-        char = char.lower()
+from threading import Thread, Lock
 
-    if char == 'w':
-        print(char)
+lock = Lock()
+user_input = ''
+done = False
+
+
+def get_user_input():
+    global done
+    global user_input
+    while not done:
+        char = getch.getch()
+        if char == ' ':
+            lock.acquire()
+            user_input = char
+            done = True
+            lock.release()
+        else:
+            char = char.lower()
+        lock.acquire()
+        user_input = char
+        lock.release()
+
+
+def execute_user_input():
+    global done
+    global user_input
+    while not done:
+        lock.acquire()
+        input = user_input
+        lock.release()
+        if input == 'w':
+            print(user_input)
+
+
+thread_reader = Thread(target=get_user_input)
+thread_executer = Thread(target=execute_user_input)
+
+thread_reader.start()
+thread_executer.start()
+thread_reader.join()
+thread_executer.join()
